c5e39d6040b84f4ecd44f38872391d91248489ae
NAGARJUNMAMSE2021 <bnarjun.csemamse2021@gmail.com>
2025-04-13 01:47:17 -0700

    .java

--- Vowel1.java
+++ Vowel1.java
@@ -4,7 +4,7 @@ public static void main(String[]args)
 {
 String str="java is a programming language";
 String[]s1=str.split("");
-String s1=s1.replaceAll("[aeiouAEIOU]","");
+String s2=s1.replaceAll("[aeiouAEIOU]","");
 for(int i=0;i<s2.length;i++)
 {
 reverse(s2[i]);
@@ -13,9 +13,9 @@ reverse(s2[i]);
 public static void reverse(String str)
 {
 String result="";
-for(int i=<s2.length;i<=0;i--)
+for(int i=0;i<s2.length();i++)
 {
-result=result+s2.charAt[i];
+result=s2.charAt[i]+result;
 }
 System.out.println(result);
 }
